99990f970b91cf473ac2d75e45f06f455542c94f
Milan Veenstra <milanveenstra27@gmail.com>
2023-03-28 22:38:00 +0200

    Eerste commit, base template gemaakt

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def Home():
+    title = "Home"
+    css_route = "css/home.css"
+    return render_template("home.html", title=title, css_route=css_route)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
